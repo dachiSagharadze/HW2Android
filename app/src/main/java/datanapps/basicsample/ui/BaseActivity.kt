f60629d76b39957e9258b34331fef389c2585b54
dachiSagharadze <dachi.sagaradze@gmail.com>
2024-12-24 12:38:10 +0400

    Add files via upload

---
+++ app/src/main/java/datanapps/basicsample/ui/BaseActivity.kt
@@ -0,0 +1,10 @@
+package datanapps.basicsample.ui
+
+import android.os.Bundle
+
+open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
